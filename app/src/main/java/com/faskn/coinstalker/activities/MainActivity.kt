a29f6f66e1a7b2a69046ab19096ff4a07f8cf684
faskN <seeingisknow@gmail.com>
2018-12-28 16:28:05 +0300

    Added Bottom Navigation.

--- app/src/main/java/com/faskn/coinstalker/activities/MainActivity.kt
+++ app/src/main/java/com/faskn/coinstalker/activities/MainActivity.kt
@@ -4,14 +4,17 @@ import android.os.Bundle
 import android.os.CountDownTimer
 import android.view.MenuItem
 import android.view.View
+import androidx.fragment.app.FragmentTransaction
 import com.faskn.coinstalker.R
 import com.faskn.coinstalker.base.BaseActivity
+import com.faskn.coinstalker.fragments.CoinsFragment
+import com.faskn.coinstalker.fragments.ConverterFragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
-import kotlinx.android.synthetic.main.fragment_splash.*
 
-class MainActivity : BaseActivity(),BottomNavigationView.OnNavigationItemReselectedListener {
+class MainActivity : BaseActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
 
-    val bottomNav by lazy {  findViewById<BottomNavigationView>(R.id.bottom_navigation) }
+
+    private val bottomNav by lazy { findViewById<BottomNavigationView>(R.id.bottom_navigation) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,7 +23,6 @@ class MainActivity : BaseActivity(),BottomNavigationView.OnNavigationItemReselec
 
 
         object : CountDownTimer(4000, 1000) {
-
             override fun onFinish() {
                 bottomNav.visibility = View.VISIBLE
             }
@@ -30,9 +32,23 @@ class MainActivity : BaseActivity(),BottomNavigationView.OnNavigationItemReselec
             }
 
         }.start()
+
+        bottomNav.setOnNavigationItemSelectedListener(this)
     }
 
-    override fun onNavigationItemReselected(item: MenuItem) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        val page: Int = item.itemId
+        val transaction: FragmentTransaction = supportFragmentManager.beginTransaction()!!
+        when (page) {
+            R.id.menu_item_converter -> {
+                transaction.replace(R.id.container_fragment, ConverterFragment()).commitNow()
+            }
+            R.id.menu_item_coins -> {
+                transaction.replace(R.id.container_fragment, CoinsFragment()).commitNow()
+            }
+        }
+
+        return true
     }
+
 }
---
+++ app/src/main/java/com/faskn/coinstalker/fragments/ConverterFragment.kt
@@ -0,0 +1,23 @@
+package com.faskn.coinstalker.fragments
+
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.faskn.coinstalker.R
+
+
+class ConverterFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_converter, container, false)
+    }
+
+
+}
