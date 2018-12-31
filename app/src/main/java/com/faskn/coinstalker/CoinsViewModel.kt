b06ef5601c52a1073d787845d413e00c1df44617
faskN <seeingisknow@gmail.com>
2018-12-31 18:42:35 +0300

    Added ViewModel <3.

---
+++ app/src/main/java/com/faskn/coinstalker/CoinsViewModel.kt
@@ -0,0 +1,34 @@
+package com.faskn.coinstalker
+
+import android.app.Application
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkInfo
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
+
+
+class CoinsViewModel(application: Application) : AndroidViewModel(application) {
+
+
+    private val connectivityManager =
+        application.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+
+    private val tag = "CoinsViewModel"
+
+
+    val connectionStatusLiveData = MutableLiveData<Boolean>()
+
+    fun checkConnectionStatus() {
+
+        val activeNetwork: NetworkInfo? = connectivityManager.activeNetworkInfo
+
+        if (activeNetwork != null && activeNetwork.isConnected) {
+            connectionStatusLiveData.postValue(true)
+        } else {
+            connectionStatusLiveData.postValue(false)
+        }
+
+    }
+
+}
